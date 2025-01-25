4e31a2e59c4db5cd5c45d4476e1b8c4770f802e6
Nikos Bouklas <nik.bouklas@gmail.com>
2025-01-25 20:20:50 +0200

    Delete src/gr/aueb/cf/ch5test directory

--- src/gr/aueb/cf/ch5test/DeclarationsApp.java
+++
@@ -1,9 +0,0 @@
-package gr.aueb.cf.ch5test;
-
-public class DeclarationsApp {
-    public static void main(String[] args) {
-        final char EMPTY_CHAR = ' ';
-        char star = '*';
-        char chA = 'A';
-    }
-}
